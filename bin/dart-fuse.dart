5dcba24b3d300f2dabe53336d79af7d20da4ad04
Maksim Lin <maks@manichord.com>
2021-01-15 11:05:10 +1100

    cleanup initial POC code

--- bin/dart-fuse.dart
+++ bin/dart-fuse.dart
@@ -3,11 +3,6 @@ import 'dart:io';
 
 import 'package:ffi/ffi.dart';
 
-// FFI signature of the hello_world C function
-typedef hello_world_func = ffi.Void Function();
-// Dart type definition for calling the C foreign function
-typedef HelloWorld = void Function();
-
 // FFI signature of the set_callback C function
 typedef NativeCbFunc = ffi.Void Function(
     ffi.Pointer<ffi.NativeFunction<Callback>>);
@@ -22,18 +17,16 @@ int callbackFromNative(ffi.Pointer<Utf8> path) {
   return 1;
 }
 
-// FUSE init ==
-// FFI signature of the hello_world C function
+// FFI signature of the fuse_init C function
 typedef fuse_init_func = ffi.Void Function();
 // Dart type definition for calling the C foreign function
 typedef FuseInit = void Function();
-// == FUSE init
 
 void main(List<String> arguments) {
-  print('Hello from Dart!');
+  print('dart-fuse started');
 
   ProcessSignal.sigint.watch().listen((signal) {
-    print('time to go');
+    print('dart-fuse exited');
     exit(0);
   });
 
@@ -52,12 +45,4 @@ void main(List<String> arguments) {
       dylib.lookupFunction<fuse_init_func, FuseInit>('fuse_init');
 
   fuseInit();
-
-  // Look up the C function 'hello_world'
-  // ignore: omit_local_variable_types
-  // final HelloWorld hello = dylib
-  //     .lookup<NativeFunction<hello_world_func>>('hello_world')
-  //     .asFunction();
-  // // Call the function
-  // hello();
 }
